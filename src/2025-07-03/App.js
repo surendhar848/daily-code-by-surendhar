a67be3792c4e5d047845dfeeb490bcb0d7957c79
Surendhar Kumar D <113059857+surendhar848@users.noreply.github.com>
2025-07-03 16:45:49 +0530

    Create App.js

---
+++ src/2025-07-03/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CounterApp from './2025-07-03/CounterApp';
+
+function App() {
+  return (
+    <div>
+      <CounterApp />
+    </div>
+  );
+}
+
+export default App;
